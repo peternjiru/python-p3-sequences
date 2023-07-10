6fe442e55a447e0c8805ae11d793055011c032b7
Peter Njiru <peternjirum@gmail.com>
2023-07-09 20:15:35 -0400

    completes sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fibonacci = []
+    elif length == 1:
+        fibonacci = [0]
+    else:
+        fibonacci = [0, 1]
+        while len(fibonacci) < length:
+            next_num = fibonacci[-1] + fibonacci[-2]
+            fibonacci.append(next_num)
+    print(fibonacci)
